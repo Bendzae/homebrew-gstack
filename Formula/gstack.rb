51df533969a8b1f35fe0acf7de63f3d7e4a20eee
Ben <ben@closelink.com>
2024-07-26 14:09:43 +0200

    fix order

--- Formula/gstack.rb
+++ Formula/gstack.rb
@@ -1,9 +1,9 @@
 class GStack < Formula
   desc "CLI for utility for stacked branches and pullrequests written in Rust"
+  version "0.0.1"
   homepage "https://github.com/Bendzae/g-stack"
   url "https://github.com/Bendzae/g-stack/releases/latest/download/g-stack-0.0.1-aarch64-apple-darwin.tar.gz"
-  sha256 "4a08276b8c44f475ecd2f1e66aadbe8d37d8f6bed0e5e71a9215c0e6b058677"
-  version "0.0.1"
+  sha256 "4a08276b8c44f475ecd2f1e66aadbe8d37d8f6bed0e5e71a9215c0e6b0586774"
 
   def install
     bin.install "g-stack" => "gs"

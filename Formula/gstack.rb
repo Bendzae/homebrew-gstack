97b5c2c745ff35fba938580e1615aa0f2d5a1f27
Ben <ben@closelink.com>
2024-07-26 14:30:49 +0200

    remove version

--- Formula/gstack.rb
+++ Formula/gstack.rb
@@ -2,7 +2,6 @@ class Gstack < Formula
   desc "CLI for utility for stacked branches and pullrequests written in Rust"
   homepage "https://github.com/Bendzae/g-stack"
   url "https://github.com/Bendzae/g-stack/releases/latest/download/g-stack-0.0.1-aarch64-apple-darwin.tar.gz"
-  version "0.0.1"
   sha256 "4a08276b8c44f475ecd2f1e66aadbe8d37d8f6bed0e5e71a9215c0e6b0586774"
 
   def install

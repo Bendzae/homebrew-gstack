dd4f4c401f6f48e572abeee785b9218ab854f96e
Ben <ben@closelink.com>
2024-07-26 16:58:23 +0200

    update hash

--- Formula/gstack.rb
+++ Formula/gstack.rb
@@ -6,7 +6,7 @@ class Gstack < Formula
   desc "Cli util to simplify creating stacked branches and pull requests on github"
   homepage "https://github.com/Bendzae/g-stack"
   url "https://github.com/Bendzae/gstack/releases/download/0.0.2/gstack-0.0.2-aarch64-apple-darwin.tar.gz"
-  sha256 "e9c20925e90391c9ec837d813fc7512343ca390d1591051a860ca9232f0bd15a"
+  sha256 "4762cafff915776ea6778b2d5f5d57785d7581e69fea08e495b0353d7c648837"
   license "MIT"
 
   def install
